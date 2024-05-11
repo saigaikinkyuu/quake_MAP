bcfe2f83be45413ad8aba0f9ba74e01dfdf82046
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-05-11 19:23:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -553,7 +553,8 @@ function mapDraw(num) {
 				});
 				var shingenIcon = L.marker(shingenLatLng, {icon: shingenIconImage }).addTo(map);
 			} else {
-				map.setView("36.0","138.0", 1);
+				var initialLatLng = L.latLng(36.0, 138.0);
+				map.setView(initialLatLng, 1);
 				areaData = [0, 0]
 				var areaDataSet = new Set(areaData);
 					// GeoJSON データを読み込んで地図に追加
